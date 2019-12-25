e1e3aefaf3aca76ab39473a192fb598ddb7a84ac
HP <HP@192.168.1.210>
2019-12-25 13:00:45 +0530

    web configuration

--- GenericServlateApp1/src/com/js/date/FirstDateApp.java
+++ GenericServlateApp1/src/com/js/date/FirstDateApp.java
@@ -1,5 +1,36 @@
 package com.js.date;
 
-public class FirstDateApp {
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class FirstDateApp extends GenericServlet {
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		
+		PrintWriter pw = res.getWriter();
+		res.setContentType("text/html");
+		Date d = new Date();
+		pw.println("<html>");
+		pw.println("<body>");
+		pw.println("<h1> Date Is::"+d);
+		pw.println("</h1>");
+		pw.println("</body>");
+		pw.println("</html>");
+		
+		pw.close();
+		
+		
+		
+		
+	}
 
 }
